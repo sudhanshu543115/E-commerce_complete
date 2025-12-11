9055c1c20ecdaa8c7578414d0c45cdd77d5f6818
SUDHANSHU DUBEY <manudubey249@gmail.com>
2025-12-11 10:29:23 +0530

    Update vite.config.js

--- E-commerce_Frontend/vite.config.js
+++ E-commerce_Frontend/vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "./",  // ⭐ REQUIRED for deployment (Render, Netlify, etc.)
+  base: "./",   // ⭐ REQUIRED for deployment (Render / Netlify / GitHub Pages)
+  
   plugins: [react()],
+
   server: {
     host: true,
     port: process.env.PORT ? parseInt(process.env.PORT) : 5173
