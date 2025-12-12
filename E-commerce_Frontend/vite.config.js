c49ca710743eb5fbfc9772de85c731ca44f5b753
sudhanshu543115 <manudubey249@gmail.com>
2025-12-12 11:55:29 +0530

    api key changes 2

--- E-commerce_Frontend/vite.config.js
+++ E-commerce_Frontend/vite.config.js
@@ -1,20 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
 
 export default defineConfig({
   plugins: [
     react()
   ],
-  css: {
-    postcss: {
-      plugins: [
-        tailwindcss(),
-        autoprefixer()
-      ]
-    }
-  },
   base: "./",   // ⭐ REQUIRED for deployment (Render / Netlify / GitHub Pages)
   server: {
     host: true,
